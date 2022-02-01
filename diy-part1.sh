403bdbc1695cc2044a749a6c91e49e7c7e9223d1
aliass532 <56620619+aliass532@users.noreply.github.com>
2022-02-01 10:35:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@ sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' f
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 sed -i '3a iwconfig wlan0 txpower 24' package/base-files/files/etc/rc.local
 sed -i '4a iwconfig wlan1 txpower 24' package/base-files/files/etc/rc.local
+git clone https://github.com/aswifi/luci-theme-atmaterial_new package/luci-theme-atmaterial_new
