a3dab64bc7019b1cd9126d56f6ec5b23a78fe684
aliass532 <56620619+aliass532@users.noreply.github.com>
2022-02-02 12:16:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
-sed -i 's/channel="36"/channel="157"/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/5180 MHz/5785 MHz/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i '3a iwconfig wlan0 txpower 24' package/base-files/files/etc/rc.local
-sed -i '4a iwconfig wlan1 txpower 24' package/base-files/files/etc/rc.local
+sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial_new/g' feeds/luci/collections/luci/Makefile
