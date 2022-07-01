410ceede133e0545f9595d00ebfab65b40d48ff5
aliass532 <56620619+aliass532@users.noreply.github.com>
2022-07-02 00:54:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,9 +15,10 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 sed -i '3a iwconfig wlan0 txpower 24' package/base-files/files/etc/rc.local
 sed -i '4a iwconfig wlan1 txpower 24' package/base-files/files/etc/rc.local
-git clone https://github.com/aswifi/luci-theme-atmaterial_new package/lean/luci-theme-atmaterial_new
+#git clone https://github.com/aswifi/luci-theme-atmaterial_new package/lean/luci-theme-atmaterial_new
 #sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial_new/g' feeds/luci/collections/luci/Makefile
